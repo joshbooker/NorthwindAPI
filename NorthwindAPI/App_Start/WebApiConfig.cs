60e3e108c31a22340526e1d721cdf228ffc273be
joshbooker <email@joshuabooker.com>
2015-12-13 09:43:28 -0500

    EF_InitialCreate_Northwind; Swashbucle.OData.2.5.1

---
+++ NorthwindAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using NorthwindAPI.Models;
+using Microsoft.Restier.EntityFramework;
+using Microsoft.Restier.WebApi;
+using Microsoft.Restier.WebApi.Batch;
+
+namespace NorthwindAPI
+{
+    public static class WebApiConfig
+    {
+        public async static void Register(HttpConfiguration config)
+        {
+            await config.MapRestierRoute<DbApi<NorthwindContext>>(
+                "Restier",
+                "restier",
+                new RestierBatchHandler(GlobalConfiguration.DefaultServer));
+        }
+    }
+}
